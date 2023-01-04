a558a479d9772dcbf0dff0d254d52f5c650df17a
NTY-1017 <nty1017@gmail.com>
2023-01-04 22:19:28 +0900

    [Silver II] Title: 생태학, Time: 756 ms, Memory: 88144 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        int count = 0;
+        while (true) {
+            String tree = br.readLine();
+            if (tree == null || tree.length() == 0) break;
+            if (hm.containsKey(tree)) hm.put(tree, hm.get(tree) + 1);
+            else hm.put(tree, 1);
+            count++;
+        }
+
+        final int total = count;
+
+        hm.entrySet().stream()
+                .sorted((o1, o2) -> o1.getKey().compareTo(o2.getKey()))
+                .forEach(e -> sb.append(e.getKey()).append(" ").append(String.format("%.4f", e.getValue()*100.0/total)).append("\n"));
+
+        System.out.println(sb);
+        br.close();
+    }
+}
