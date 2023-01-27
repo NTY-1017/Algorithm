d8bdc37f8734307cc780d7105aa3420a350eb586
NTY-1017 <nty1017@gmail.com>
2023-01-28 02:32:21 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2388 ms, Memory: 356732 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Main {
     /*
     우선순위큐 2개 선언해서도 가능(두 큐 동기화 위해 체크 필요)
+    TreeMap -> firstkey는 최소 lastkey는 최대값을 갖는 key
      */
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,22 +15,22 @@ public class Main {
         while (T-- > 0) {
             TreeMap<Integer, Integer> duePQ = new TreeMap<>();
             int k = Integer.parseInt(br.readLine());
-            for (int i = 0; i < k; i++) {
+            while (k-- > 0) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 String s = st.nextToken();
-                int n = Integer.parseInt(st.nextToken());
-                if(s.equals("I")) duePQ.put(n, duePQ.getOrDefault(n, 0) + 1);
-                else {
-                    if(duePQ.isEmpty()) continue;
-                    int key = n == 1 ? duePQ.lastKey() : duePQ.firstKey();
-                    if(duePQ.get(key) == 1) duePQ.remove(key);
+                int i = Integer.parseInt(st.nextToken());
+                if(s.equals("I")) duePQ.put(i, duePQ.getOrDefault(i, 0) + 1);
+                else {  // D 일때
+                    if(duePQ.isEmpty()) continue;   // 큐 비어있으면 생략
+                    int key = i == 1 ? duePQ.lastKey() : duePQ.firstKey();  
+                    if(duePQ.get(key) == 1) duePQ.remove(key);  // 초기값이 1이므로 값이 1이면 삭제
                     else duePQ.put(key, duePQ.get(key) - 1);
                 }
             }
             if (duePQ.isEmpty()) sb.append("EMPTY").append('\n');
             else sb.append(duePQ.lastKey()).append(' ').append(duePQ.firstKey()).append('\n');
         }
-
+        
         System.out.println(sb);
     }
 }
