dea303c8b0cad1bf330c4997a3080a4380fd7440
NTY-1017 <nty1017@gmail.com>
2023-01-27 14:01:11 +0900

    [Silver I] Title: 회의실 배정, Time: 560 ms, Memory: 43468 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -5,18 +5,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<Meeting> timeLine = new ArrayList<>();
+        
         int N = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             timeLine.add(new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
-
+        
         Collections.sort(timeLine, (t1, t2) -> {
             if(t1.getEnd() != t2.getEnd()) return t1.getEnd() - t2.getEnd();
             else return t1.getStart() - t2.getStart();
         });
-
+        
         int start = 0;
         int count = 0;
         for (Meeting m : timeLine) {
@@ -28,7 +28,7 @@ public class Main {
 
         System.out.println(count);
     }
-
+    
     static class Meeting {
         int start;
         int end;
