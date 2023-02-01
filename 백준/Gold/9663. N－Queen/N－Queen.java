a7e83b8141c5efaf274cb7803cd58c36e0e4047e
NTY-1017 <nty1017@gmail.com>
2023-02-01 16:15:52 +0900

    [Gold IV] Title: N-Queen, Time: 5352 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -10,11 +10,10 @@ public class Main {
         check(0);
         System.out.println(count);
     }
-
     private static void check(int d) {
         if(d == N) count++;
         else {
-            for (int i = 0; i < N; i++) {
+            for (int i = 0; i < N; i++) {   // n 행에서 놓을 수 있는 경우 체크
                 if(isOk(d, i)){
                     column[d] = i;
                     check(d + 1);
@@ -22,11 +21,11 @@ public class Main {
             }
         }
     }
-
+    // 체스 놓을 수 있는지 체크
     private static boolean isOk(int r, int c) {
         for (int i = 0; i < r; i++) {
-            if(column[i] == c) return false;
-            if(Math.abs(i - r) == Math.abs(column[i] - c)) return false;
+            if(column[i] == c) return false;    // 같은 열에 있는 경우 체크 
+            if(Math.abs(i - r) == Math.abs(column[i] - c)) return false;    // 대각선에 있는 경우 체크
         }
         return true;
     }
