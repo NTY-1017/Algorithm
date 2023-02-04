d584d14a751ded439963d7633475793ebfa7fa40
Noh Taeyun <nty1017@gmail.com>
2023-02-04 20:35:36 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,19 +1,16 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        int number = 0;
-        int count = 0;
+        int[] result = new int[]{0, 0};
         Set<String> set = new HashSet<>();
         set.add(words[0]);
-        
         for(int i = 1; i < words.length; i++) {
             if(set.contains(words[i]) || words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
-                number = (i + 1) % n == 0 ? n : (i + 1) % n;
-                count = i / n + 1;
+                result[0] = (i + 1) % n == 0 ? n : (i + 1) % n;
+                result[1] = i / n + 1;
                 break;
             } else set.add(words[i]);
         }
-
-        return new int[]{number, count};
+        return result;
     }
 }
