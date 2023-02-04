03b878e304f14188a388637a8f188fb7f1cd37c7
Noh Taeyun <nty1017@gmail.com>
2023-02-04 22:14:01 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,9 +1,6 @@
-
-
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        
         for(int i = 0; i < skill_trees.length; i++) {
             int idx = -1;
             loop:
@@ -17,7 +14,6 @@ class Solution {
                 if(j == skill_trees[i].length() - 1) answer++;
             }
         }
-        
         return answer;
     }
 }
