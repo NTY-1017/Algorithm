6db0c4104abd3916688f0a85350aecf607f1a01f
Noh Taeyun <nty1017@gmail.com>
2023-02-07 12:42:49 +0900

    [Silver I] Title: 신입 사원, Time: 1756 ms, Memory: 302968 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -12,15 +12,12 @@ public class Main {
             List<int[]> applicant = new ArrayList<>();
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                int paper = Integer.parseInt(st.nextToken());
-                int interview = Integer.parseInt(st.nextToken());
-                applicant.add(new int[]{paper, interview});
+                applicant.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
             }
-            Collections.sort(applicant, (a1, a2) -> a1[0] - a2[0]);
-
+            Collections.sort(applicant, (a1, a2) -> a1[0] - a2[0]);    // 서류기준 정렬
             int min = applicant.get(0)[1];
             int fail = 0;
-            for (int i = 1; i < N; i++) {
+            for (int i = 1; i < N; i++) {    // 인터뷰 최소 등수 갱신 or 탈락
                 int interview = applicant.get(i)[1];
                 if(interview < min) min = interview;
                 else fail++;
