4b2895585026860ae901fcd5a46a2d0cee8c5594
Noh Taeyun <nty1017@gmail.com>
2023-02-07 18:39:02 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 560 ms, Memory: 52008 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -6,17 +6,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
+
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) pq.add(Integer.parseInt(st.nextToken()));
-
-            int root = pq.poll();
-            int left = root;
-            int right = root;
-            int level = 0;
+            // 초기값 설정
+            int root = pq.poll(); 
+            int left = pq.poll();
+            int right = pq.poll();
+            int level = Math.abs(root - right);
             while (!pq.isEmpty()) {
                 if(pq.size() == 1) level = Math.max(Math.abs(left - pq.poll()), level);
                 else {
