83b9a7db53b110cce39a3289d6bede9f254f258d
Noh Taeyun <nty1017@gmail.com>
2023-02-09 16:59:40 +0900

    [level 3] Title: 기지국 설치, Time: 0.63 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int idx = 1;
+        for(int i = 0; i < stations.length; i++) {
+            if(stations[i] - w > idx) answer += 1 + (stations[i] - w - idx - 1) / (2 * w + 1) ;
+            idx = stations[i] + w + 1; 
+        }
+        if(idx <= n) answer += 1 + (n - idx) / (2 * w + 1) ;
+        return answer;
+    }
+}
