a47132f8cd618aa8bf71d78f8ef2891963403219
Noh Taeyun <nty1017@gmail.com>
2023-03-09 23:55:48 +0900

    [level 2] Title: 배달, Time: 0.95 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.java
+++ 프로그래머스/lv2/12978. 배달/배달.java
@@ -2,14 +2,13 @@ import java.util.*;
 class Solution {
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-
         int[][] graph = new int[N][N];
-        
+    
         for(int i = 0; i < N; i++) {
             Arrays.fill(graph[i], 2000 * (K-1));
             graph[i][i] = 0;
         }
-        
+    
         for(int i = 0; i < road.length; i++) {
             int a = road[i][0] - 1;
             int b = road[i][1] - 1;
@@ -20,9 +19,7 @@ class Solution {
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> p1[1] - p2[1]);
         boolean[] check = new boolean[N];
-        for(int i = 0; i < N; i++) {
-            if(graph[0][0] != 2000 * (K-1)) pq.add(new int[]{i, graph[0][i]});
-        }
+        for(int i = 0; i < N; i++) if(graph[0][0] != 2000 * (K-1)) pq.add(new int[]{i, graph[0][i]});
         check[0] = true;
         while(!pq.isEmpty()) {
             int[] head = pq.poll();
